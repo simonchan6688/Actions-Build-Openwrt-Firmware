4d4dc506d763e92a2e873185e9c527f2d25499bb
simonchan6688 <34909952+simonchan6688@users.noreply.github.com>
2020-12-24 16:48:12 +0800

    change the default passwd

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+
+# modify default password
+sed -i 's/root::0:0:99999:7:::/root:$1$xYjM6fLJ$5x0lnNu29mqp9h1TYKV961:18620:0:99999:7:::/g' package/base-files/files/etc/shadow
