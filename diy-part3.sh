85af47c700e16915ac0c6241d5ba2e6c43fb6720
simonchan6688 <34909952+simonchan6688@users.noreply.github.com>
2021-02-05 12:26:41 +0800

    Create diy-part3.sh

---
+++ diy-part3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part3.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# fw876/helloworld
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+git clone https://github.com/fw876/helloworld package/lean/helloworld
+# Lienol/openwrt-package
+# sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
+# sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-package' feeds.conf.default
+
+# oaf
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
+# jd-dailybonus
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
