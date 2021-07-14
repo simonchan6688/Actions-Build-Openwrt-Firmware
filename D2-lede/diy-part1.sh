c250a4fcbbd8e1c5959986b5c1be6c7b2275c263
simonchan6688 <34909952+simonchan6688@users.noreply.github.com>
2021-07-14 12:48:20 +0800

    Update diy-part1.sh

--- D2-lede/diy-part1.sh
+++ D2-lede/diy-part1.sh
@@ -9,8 +9,8 @@
 #=============================================================
 
 # fw876/helloworld
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-# git clone https://github.com/fw876/helloworld package/lean/helloworld
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+git clone https://github.com/fw876/helloworld package/lean/helloworld
 # Lienol/openwrt-package
 # sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
 # sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-package' feeds.conf.default
